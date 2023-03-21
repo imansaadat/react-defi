c1bf821731c8fd81c522d16d1fc4abdf0c226843
Eslam Saadat (Iman) <80543205+eslamsaadat@users.noreply.github.com>
2023-03-21 14:11:45 +0330

    navbar component updated

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -1,38 +1,12 @@
-import React from 'react'
-import { Link } from 'react-scroll';
+import React from "react";
+import { Link } from "react-scroll";
 
-const NavLinks = ({handelMenu}) => {
-  const Links = [
-    {
-      name: 'Platform',
-      path: 'platform'
-    },
-    {
-      name: 'Developers',
-      path: 'developers'
-    },
-    {
-      name: 'Community',
-      path: 'subscribe'
-    },
-    {
-      name: 'About',
-      path: 'about'
-    }
-  ]
+const NavLinks = ({ item: {name,path},handelMenu}) => {
   return (
-    <>
-      {
-        Links.map((link, index) => (
-          <li key={index} className='list_item'>
-            <Link to={link.path} smooth={true} duration={500} onClick={handelMenu}>
-              {link.name}
-            </Link>
-          </li>
-        ))
-      }
-    </>
-  )
-}
+    <li className='list_item'>
+      <Link to={path} smooth={true} duration={500} onClick={handelMenu}>{name}</Link>
+    </li>
+  );
+};
 
-export default NavLinks
+export default NavLinks;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,63 @@
+import React, { useState } from "react";
+import { VscThreeBars } from "react-icons/vsc";
+import { VscClose } from "react-icons/vsc";
+import NavLinks from "./NavLinks";
+import Button from "../Button/Button";
+import "./Navbar.css";
 import React, { useState } from 'react'
 import {VscThreeBars} from 'react-icons/vsc'
 import {VscClose} from 'react-icons/vsc'
 import NavLinks from './NavLinks'
 import Button from '../Button/Button'
-import { Link } from 'react-scroll';
 import './Navbar.css'
 const Navbar = () => {
-  const [open,setOpen] = useState(false)
+  const Links = [
+    {
+      name: "Platform",
+      path: "platform",
+    },
+    {
+      name: "Developers",
+      path: "developers",
+    },
+    {
+      name: "Community",
+      path: "subscribe",
+    },
+    {
+      name: "About",
+      path: "about",
+    },
+  ];
+  const [open, setOpen] = useState(false);
 
-  const handelMenu = ()=>{
-    setOpen(!open)
-  }
+  const handelMenu = () => {
+    setOpen(!open);
+  };
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='header_content'>
-          <h1 className='logo_name'>Defi</h1>
-          <nav className={open ? 'navbar active' : 'navbar'}>
-            <ul className='nav_lists'>
-              <NavLinks handelMenu={handelMenu} />
+    <header className="header">
+      <div className="container">
+        <div className="header_content">
+          <h1 className="logo_name">Defi</h1>
+          <nav className={open ? "navbar active" : "navbar"}>
+            <ul className="nav_lists">
+              {Links.map((item, index) => (
+                <NavLinks item={item} key={index} handelMenu={handelMenu} />
+              ))}
             </ul>
-            <Button text='Use Defi' BtnType='btn' />
+            <Button text="Use Defi" BtnType="btn" />
           </nav>
-          <div className='hamMenu' onClick={handelMenu}>
-              {open ? (<VscClose className='icon'/>):( <VscThreeBars className='icon'/>)}
+          <div className="hamMenu" onClick={handelMenu}>
+            {open ? (
+              <VscClose className="icon" />
+            ) : (
+              <VscThreeBars className="icon" />
+            )}
           </div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
